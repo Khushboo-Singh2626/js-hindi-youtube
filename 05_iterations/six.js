b7f3218f3a0a79e6be74520a6dbce6613518081d
Khushboo-Singh2626 <khushboosingh9369@gmail.com>
2024-09-25 16:45:52 +0530

    Update six.js

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -10,7 +10,7 @@
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNums.filter( (num) => {
+// const newNums = myNums.filter( (num) => { agr curly braces lga diya to scope start ho gya hai to ab return keyword use krna hi pdega
 //     return num > 4
 // } )
 
